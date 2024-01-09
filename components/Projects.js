21d6581c22b51ba94ef6531da8a5e1fd78ee070b
Andrew <mullinsa428@gmail.com>
2024-01-09 15:26:58 -0500

    mobile styling 1

--- components/Projects.js
+++ components/Projects.js
@@ -2,19 +2,12 @@ import projectsArr from "@/assets/projects.json";
 import styles from "../app/page.module.css";
 
 const Project = ({ title, image, description, technology, deployLink, repoLink }) => {
-    const moreStyle = {
-        overflowY: "scroll",
-        maxHeight: "40%",
-    };
-
     return (
         <div className={styles.projDiv}>
             <img src={image} width="200" height="200" />
             <div className={styles.projText}>
                 <div className={styles.smallTitle}>{title}</div>
-                <div className={styles.genText} style={moreStyle}>
-                    {description}
-                </div>
+                <div className={styles.projDesc}>{description}</div>
                 <div>
                     <a href={repoLink} target="_blank" className={styles.projLinks}>
                         GitHub
